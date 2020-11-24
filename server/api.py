475d32009e977dd4c4f621f77ea7c933c98ad438
Saleh Hassen <sih28@cornell.edu>
2020-11-23 22:27:50 -0500

    rushed commit to push changes, in case needed, wont be able to access laptop for awhile

--- server/api.py
+++ server/api.py
@@ -1,5 +1,11 @@
 import flask
+import socket
 from IFNCTT_request import *
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import os
+
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
@@ -18,6 +24,22 @@ def daily_check():
         response_code = 200
     return flask.jsonify(response, response_code)
 
+def get_this_addr():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    this_addr = s.getsockname()[0]
+    s.close()
+    return this_addr
 
+# Initialization
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': os.environ.get("IFNCTT_GC_PROJECT_ID"),
+})
+
+db = firestore.client()
+
+print(get_this_addr())
 app.run(host='0.0.0.0')
 
